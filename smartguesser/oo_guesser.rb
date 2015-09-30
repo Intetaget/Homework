8ce545cd5084b303f54c9f62321cd819ba009c78
Mans Nilsson <intetaget>
2015-09-30 13:19:05 -0400

    Added files

--- smartguesser/oo_guesser.rb
+++ smartguesser/oo_guesser.rb
@@ -1,7 +1,7 @@
-#require "pry"
 require "./human"
 require "./random"
 require "./counting"
+require "./smart"
 
 class GuessingGame
   def initialize(player)
@@ -10,23 +10,30 @@ class GuessingGame
 
   def play
     number = rand(1..100)
-    guess = @player.get_guess
+    result = nil
+    guess = @player.get_guess(result)
     count = 1
     until guess == number
       if guess > number
         puts "Too High!"
+        result = ??
       else
         puts "Too Low!"
+        result = ??
       end
-      guess = @player.get_guess
+      guess = @player.get_guess(result)
       count += 1
     end
     puts "You win! Took #{count} tries. The number was #{number}."
   end
 end
 
-game = GuessingGame.new(CountingPlayer.new)
+game1 = GuessingGame.new(CountingPlayer.new)
+# game2 = GuessingGame.new(RandomPlayer.new)
+# game3 = GuessingGame.new(RandomPlayer.new)
+# game4 = GuessingGame.new(HumanPlayer.new)
 
+game1.play
 
 
 puts "just screwing around"
---
+++ smartguesser/smart.rb
@@ -0,0 +1,51 @@
+STARTNUMBER = 50
+class smart
+  	number = rand(1..100)
+  		until guess == number
+  			if STARTNUMBER < number
+  			then 1.5(STARTNUMBER)
+				else (STARTNUMBER)/1.5
+				end
+end
+
+class smartplayer
+  def initialize
+    @guess=50
+  end
+
+
+
+
+
+
+
+
+ 
+    #@count = 0
+
+  # def play
+  #   number = rand(1..100)
+  #   result = nil
+  #   guess = 50(result)
+  #   action1 = 1
+  #   until guess == number
+  #     if guess > number
+  #       guess/2
+  #       result = ??
+  #     else
+  #       puts "Too Low!"
+  #       result = ??
+  #     end
+  #     guess = @player.get_guess(result)
+  #     count += 1
+  #   end
+  #   puts "You win! Took #{count} tries. The number was #{number}."
+
+
+
+
+# Then add a new file smart.rb containing a SmartPlayer class. 
+# The new player should make an intelligent to decision about how to guess based on the 
+# result of the last guess. Note that this requires tweaking the definition of get_guess 
+# to take an argument in each player class and that the result of the last guess must be 
+# passed from the GuessingGame class.
