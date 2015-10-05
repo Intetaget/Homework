628b7432d6824dffda02c6ff791cf22d3232e337
Mans Nilsson <intetaget>
2015-10-05 16:48:32 -0400

    just gittin it

--- tester/tests.rb
+++ tester/tests.rb
@@ -1,40 +1,20 @@
+require './board'
+
 require 'minitest/autorun'
-require "pry"
 
-require './human'
-require './coffee'
+class BoardTests < MiniTest::Test
+	def test_can_build_a_board
+		assert Board.new
+	end
 
-## This assignment made more sense when I wrote it in the morning.
-class CaffeineTest < MiniTest::Test
-  def test_humans_tend_to_be_sleepy
-    tyler = Human.new "Tyler"
-    assert tyler.alertness < 0.1
-  end
+	def test_can_make_move
+		board = Board.new
+		board.move!(5, "X")
+		assert board.valid_move?(6)
+		refute board.valid_move?(5)#checking for input!!!!
+	end
 
-  def test_humans_need_coffee
-    randy = Human.new "Randy"
-    refute randy.has_coffee?
-    assert randy.needs_coffee?
-  end
-
-  def test_humans_can_drink_coffee
-    sherri = Human.new "Sherri"
-    tsmf = Coffee.new "Triple Shot Mocha Frappuccino"
-    assert tsmf.full?
-
-    sherri.buy(tsmf)
-    sherri.drink!
-    assert_in_epsilon sherri.alertness, 0.33, 0.1
-    refute tsmf.full?
-    refute tsmf.empty?
-  end
-
-  def test_humans_can_drink_all_the_coffee
-    trevor = Human.new "Trevor"
-    tsmf = Coffee.new "Triple Shot Mocha Frappuccino"
-    trevor.buy tsmf
-    3.times { trevor.drink! }
-    assert tsmf.empty?
-    assert trevor.alertness > 0.9
-  end
+	def test_can_detect_valid_move
+      # TODO: If a space isn't taken, it's valid.
+	end
 end
